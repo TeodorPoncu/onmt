a742896dfbd41201dde5aaad59bfddc6d4602f3d
ponku <ripziz@hotmail.com>
2019-08-27 12:04:00 +0300

    v4

--- config/models/sketch_transformer.py
+++ config/models/sketch_transformer.py
@@ -5,17 +5,16 @@ def model():
     return onmt.models.Transformer(
         source_inputter=onmt.inputters.WordEmbedder(
             vocabulary_file_key="source_words_vocabulary",
-            embedding_size=512,
+            embedding_size=256,
             dropout=0.1
         ),
         target_inputter=onmt.inputters.WordEmbedder(
             vocabulary_file_key="target_words_vocabulary",
-            embedding_size=512,
+            embedding_size=256,
             dropout=0.1
         ),
-        num_layers=6,
-        num_units=512,
+        num_layers=4,
+        num_units=256,
         num_heads=8,
-        ffn_inner_dim=2048,
-        share_embeddings=onmt.models.EmbeddingsSharingLevel.ALL,
+        ffn_inner_dim=1024
     )

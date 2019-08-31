3f7216fd167b6178136840f93883ae682956a1d9
ponku <ripziz@hotmail.com>
2019-09-01 02:20:16 +0300

    vmu23

--- config/models/multi_source_transformer.py
+++ config/models/multi_source_transformer.py
@@ -172,23 +172,23 @@ class MultiSourceTransformer(CustomTransformer):
       source_inputter=onmt.inputters.ParallelInputter([
           onmt.inputters.WordEmbedder(
               vocabulary_file_key="source_vocabulary_1",
-              embedding_size=256),
+              embedding_size=512),
           onmt.inputters.WordEmbedder(
               vocabulary_file_key="source_vocabulary_2",
-              embedding_size=256),
+              embedding_size=512),
           onmt.inputters.WordEmbedder(
               vocabulary_file_key="source_vocabulary_3",
-              embedding_size=256),
+              embedding_size=512),
           onmt.inputters.WordEmbedder(
               vocabulary_file_key="source_vocabulary_4",
-              embedding_size=256)]),
+              embedding_size=512)]),
       target_inputter=onmt.inputters.WordEmbedder(
           vocabulary_file_key="target_vocabulary",
           embedding_size=256),
       num_layers=4,
       num_units=256,
-      num_heads=8,
-      ffn_inner_dim=1024,
+      num_heads=6,
+      ffn_inner_dim=512,
       dropout=0.1,
       attention_dropout=0.1,
       relu_dropout=0.1,

c73ae3c1bc85f7771a2f6e9f57d8df9b05c1e704
ponku <ripziz@hotmail.com>
2019-08-31 23:54:36 +0300

    v156

--- opennmt/models/__init__.py
+++ opennmt/models/__init__.py
@@ -2,6 +2,7 @@
 
 from opennmt.models.language_model import LanguageModel
 from opennmt.models.transformer import Transformer
+from opennmt.models.custom_transformer import CustomTransformer
 from opennmt.models.sequence_to_sequence import SequenceToSequence, EmbeddingsSharingLevel
 from opennmt.models.sequence_tagger import SequenceTagger
 from opennmt.models.sequence_classifier import SequenceClassifier
